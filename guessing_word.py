9496fe6f16a5ef651b462587a57e77ccd73255af
Techshubham45 <149092799+Shubham45264@users.noreply.github.com>
2024-06-24 18:31:44 +0530

    Update guessing_word.py

--- guessing_word.py
+++ guessing_word.py
@@ -1,25 +1,41 @@
 import random
-f=open("words.txt", "r")
-data=f.readline()
-words=data.split()
-word=random.choice(words)
-#words = ["UMBRELLA","COMPUTER","TELESCOPE","SMARTPHONE"]
-word = random.choice(words)
-total_chances = 7
-guessed_word = "-"*len(word)
-while total_chances != 0:
-  print(guessed_word)
-  letter = input("Enter a letter: ").upper()
-  if letter in word:
-   for index in range(len(word)):
-      if word[index] == letter:
-        guessed_word = guessed_word[:index] + letter + guessed_word[index+1:]
-     # print(guessed_word)
-  if guessed_word == word:
-    print("Congratulations, you won!")
-    break
+
+animals = {'elephant', 'tiger', 'lion', 'zebra', 'giraffe', 'monkey', 'crocodile', 'kangaroo'}
+fruits = {'apple', 'banana', 'orange', 'grapes', 'watermelon', 'pineapple', 'mango', 'strawberry'}
+stationary = {'pencil', 'pen', 'eraser', 'sharpener', 'ruler', 'scale', 'compass', 'calculator'}
+
+# Combine all sets into a list
+words = list(animals) + list(fruits) + list(stationary)
+
+random_word = random.choice(words)
+print("Word Guessing Game")
+if random_word in animals:
+    print("Hint: The word is an animal")
+elif random_word in fruits:
+    print("Hint: The word is a fruit")
 else:
-  print("Game Over")
-  print("You Lose")
-  print("All the chances are over")
-print("The correct word is",word)
+    print("Hint: The word is stationary")
+
+user_guesses = ''
+chances = 5
+
+while chances > 0:
+    wrong_guesses = 0
+    for ch in random_word:
+        if ch in user_guesses:
+            print(ch, end=' ')
+        else:
+            print('_', end=' ')
+            wrong_guesses += 1
+
+    if wrong_guesses == 0:
+        print('\nCongratulations, you won! The word is', random_word)
+        break
+
+    guess = input("\nMake a guess: ")
+    user_guesses += guess.lower()  # Convert guess to lowercase to handle case insensitivity
+    if guess.lower() not in random_word:
+        chances -= 1
+        print(f"Wrong. You have {chances} more chances left.")
+        if chances == 0:
+            print("Game over. You lose. The word was", random_word)
